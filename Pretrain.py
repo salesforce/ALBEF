90541c975b01150ba8a1dfb880d3adb8bcd3af8b
root <ljn12260409@gmail.com>
2021-07-15 23:36:18 +0000

    update

--- Pretrain.py
+++ Pretrain.py
@@ -1,3 +1,10 @@
+'''
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+'''
+
 import argparse
 import os
 import ruamel_yaml as yaml
--- models/model_pretrain.py
+++ models/model_pretrain.py
@@ -1,3 +1,10 @@
+'''
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+'''
+
 from functools import partial
 from models.vit import VisionTransformer, interpolate_pos_embed
 from models.xbert import BertConfig, BertForMaskedLM
