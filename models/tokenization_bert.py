6224e78e5292757ca9c7d5add3e415010955a58b
Junnan Li <junnan.li@salesforce.com>
2022-04-25 20:25:35 +0800

    Update tokenization_bert.py

--- models/tokenization_bert.py
+++ models/tokenization_bert.py
@@ -291,7 +291,7 @@ class BertTokenizer(PreTrainedTokenizer):
 
         if token_ids_1 is not None:
             return [1] + ([0] * len(token_ids_0)) + [1] + ([0] * len(token_ids_1)) + [1]
-        return [1] + ([0] * len(token_ids_0)) + [1]
+        return [1] + ([0] * len(token_ids_0))
 
     def create_token_type_ids_from_sequences(
         self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
